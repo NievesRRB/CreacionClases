fc427d0f4d9de7d0998780976a605cc2a771633a
NievesRRB <37862447+NievesRRB@users.noreply.github.com>
2018-09-09 22:05:57 +0200

    Add files via upload

---
+++ Valoraciones/Valoraciones/LibroValoraciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valoraciones
+{
+    class LibroValoraciones
+    {
+        //Creo mi listado de valoraciones, donde se van a almacenar las valoraciones
+        List<float> valoraciones = new List<float>();
+
+        //Declaro mi constructor. Todas las clases tienen un constructor.
+        public LibroValoraciones()
+        {
+            valoraciones = new List<float>();
+        }
+        
+        //Declaramos metodos de mi clase
+        public void AgregarValoracion(float valoracion)
+        {
+            valoraciones.Add(valoracion);
+        }
+
+        
+    }
+}
---
+++ Valoraciones/Valoraciones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valoraciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Invoco al constructor para crear un objeto 
+            LibroValoraciones libro = new LibroValoraciones();
+            libro.AgregarValoracion(3.5f);
+            libro.AgregarValoracion(5);
+
+            //Construyo un objeto sobre la variable de tipo libro, creada inicialmente como un objeto de clase 
+            libro = new LibroValoraciones();
+            libro.AgregarValoracion(2);
+
+            //Hay que tener cuidado cuando creamos una variable como un objeto de clase a cuando utilizo la variable para 
+            //almacenar datos. 
+
+            //Asigno valores de una variable a otra varible siempre y cuando sean del mismo tipo, ya q sino me da error
+            LibroValoraciones libro2 = libro;
+
+            //Lo q no podria hacer seria la siguiente conversion, ya q estoy asignando un entero a mi varible del tipo
+            //LibroValoraciones
+            LibroValoraciones libro3 = 10;
+
+            //Lo que si puedo hacer es llamar a mi metodo de AgregarValoracion 
+            libro.AgregarValoracion(2);
+        }
+    }
+}
